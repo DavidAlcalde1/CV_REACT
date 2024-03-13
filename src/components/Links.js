24c06ce6af229e95585341c8d96a2942557ba674
José David Alcalde Cabrera <85404854+DavidAlcalde1@users.noreply.github.com>
2024-03-13 16:08:46 -0500

    Instalación de framer-motion para efectos

--- src/components/Links.js
+++ src/components/Links.js
@@ -2,22 +2,23 @@ import { FaFacebook } from "react-icons/fa";
 import { GrGithub } from "react-icons/gr";
 import { FaLinkedin } from "react-icons/fa";
 import { FaWhatsappSquare } from "react-icons/fa";
+import '../styles/stylesLinks.css'
 
 
 function Links() {
     const numberWhatsApp = '+51946559747'; 
     return(
         <div className="links">
-            <a href="https://www.facebook.com/DavidAlcaldeC" target="blank" rel="noopener noreferrer">
-            <FaFacebook size={30} color="#3b5998"/>
+            <a className="fb" href="https://www.facebook.com/DavidAlcaldeC" target="blank" rel="noopener noreferrer">
+            <FaFacebook size={30} color="#0866FF"/>
             </a>
-            <a href="https://github.com/DavidAlcalde1" target="blank" rel="noopener noreferrer">
-            <GrGithub size={30} color="#4078c0"/>
+            <a className="git" href="https://github.com/DavidAlcalde1" target="blank" rel="noopener noreferrer">
+            <GrGithub size={30} color="gray"/>
             </a>
-            <a href="https://www.linkedin.com/in/jos%C3%A9-david-alcalde-cabrera-068b5a7a/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin size={30} color="#0077B5"/>
+            <a className="linke" href="https://www.linkedin.com/in/jos%C3%A9-david-alcalde-cabrera-068b5a7a/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={30} color="#0A66C2"/>
             </a>
-            <a href={`https://wa.me/${numberWhatsApp}`} target="_blank" rel="noopener noreferrer">
+            <a className="wa" href={`https://wa.me/${numberWhatsApp}`} target="_blank" rel="noopener noreferrer">
             <FaWhatsappSquare size={30} color='#25D366' />        
             </a>
         </div>
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {motion} from 'framer-motion'
 import '../styles/stylesPhoto.css';
 
 function Photo(){
+    const [isHovered, setIsHovered] = React.useState(false);
+    
     return(
         <div className='photo'>
-            <img className='imagePhoto' src= "./images/fotoDavid.jpeg" alt = "foto mía" width= "250rem" />
+            <motion.img className='imagePhoto'
+            animate={{ filter : isHovered ? "brightness(1.2)" : "brightness(1)" } }
+            whileHover={{ filter: "brightness(1.2)" }} 
+            // animate={{scale : isHovered ? 1.2 : 1}}
+            // whileHover={{ scale : 1.2}}  
+            src= "./images/fotoDavid.jpeg" alt = "foto mía" width= "250rem" />
         </div>
     );
 }
